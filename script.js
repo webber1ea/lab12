9c9db4ae5fb4e267dee50c9e5bb922371af42aa0
Emily Webber <webber.emilya@gmail.com>
2017-07-18 21:03:49 -0400

    changed background color to IMG, added style to DIV

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var app = angular.module("lab12", []);
 
 app.controller('lab12Ctrl', function($scope){
 
-$scope.toDoList = [ 'Go Grocery Shopping', 'Clean Bedroom' ];
+$scope.toDoList = [ 'Go Grocery Shopping', 'Clean Bedroom', 'Clean Garage', 'Mop Kitchen Floor', 'Do Homework', 'Study for test', 'Give dog a bath', 'Tend to Garden' ];
 
 $scope.addTo = function(usersTask){
   $scope.toDoList.push(usersTask);
