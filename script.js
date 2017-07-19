665ee094eb82970941a155e5f47cc4e42778db77
Emily Webber <webber.emilya@gmail.com>
2017-07-18 21:35:35 -0400

    changed theme to SpongeBob

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ var app = angular.module("lab12", []);
 
 app.controller('lab12Ctrl', function($scope){
 
-$scope.toDoList = [ 'Go Grocery Shopping', 'Clean Bedroom', 'Clean Garage', 'Mop Kitchen Floor', 'Do Homework', 'Study for test', 'Give dog a bath', 'Tend to Garden' ];
+$scope.toDoList = [ 'Eat Ice Cream', 'Eat at the Krusty Krab', 'Visit Squidward', 'Jellyfish with Sandy', 'Sleep', 'Nap', 'Swim at the Blue Lagoon', 'Rest', 'Wait for SpongeBob to get home'];
 
 $scope.addTo = function(usersTask){
   $scope.toDoList.push(usersTask);
+};
+
+
+$scope.remove = function(index){
+  $scope.toDoList.splice(index, 1)
 };
 });
